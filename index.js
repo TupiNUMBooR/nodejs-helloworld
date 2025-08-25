4bff2fb79aede9db7bdb629a22eb475a21f6dac6
TupiNUMBooR@Wolf <tupinumboor@gmail.com>
2025-08-25 17:57:25 +0300

    Remove shutdown delay

--- index.js
+++ index.js
@@ -4,5 +4,14 @@ require('dotenv').config();
 const http = require('http');
 const port = process.env.PORT || 3000;
 
-http.createServer((req, res) => res.end('hello world'))
-  .listen(port, () => console.log(`http://localhost:${port}`));
+const server = http.createServer((req, res) => res.end('hello world'));
+
+function shutdown(signal) {
+  console.log('shutdown on ' + signal);
+  server.close();
+  console.log('shutdown');
+}
+
+server.listen(port, () => console.log(`http://localhost:${port}`));
+process.on('SIGTERM', shutdown);
+process.on('SIGINT',  shutdown);
