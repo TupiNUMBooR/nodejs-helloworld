8157cb965f1e17d89422efc58ceb5787b09b6b3f
TupiNUMBooR@Wolf <tupinumboor@gmail.com>
2025-08-28 00:32:24 +0300

    Add error handling

--- index.js
+++ index.js
@@ -8,20 +8,31 @@ const port = process.env.PORT || 3000;
 let server;
 
 function start() {
+  handleSignals();
   server = http.createServer((req, res) => {
     httpLogger(req, res);
     res.end('hello world');
   });
   server.listen(port, () => console.log(`http://localhost:${port}`));
+  logger.info('Server is running on port ' + port);
+}
+
+function handleSignals() {
   process.on('SIGTERM', shutdown);
   process.on('SIGINT',  shutdown);
-  logger.info('Server is running on port ' + port);
+  process.on('unhandledRejection', e => shutdownOnError('unhandledRejection', e));
+  process.on('uncaughtException', e => shutdownOnError('uncaughtException', e));
 }
 
 function shutdown(signal) {
   logger.info('shutdown on ' + signal);
   server.close();
-  logger.info('shutdown');
+}
+
+function shutdownOnError(errorType, error) {
+  logger.error('shutdown on '+ errorType +': "' + error + '"');
+  server.close();
+  process.exit(101);
 }
 
 start();
